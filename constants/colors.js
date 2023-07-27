746b63d2076ad9ca30aebf0a95fb7fe06a9e1de9
Vedant Bakshi <vedantbakshi07@gmail.com>
2023-07-27 20:45:32 +0530

    added welcome screen & settiung up themes

--- constants/colors.js
+++ constants/colors.js
@@ -3,7 +3,8 @@ const COLORS = {
     black: "#222222",
     primary: "#a2d2ff",
     secondary: "#39B68D",
-    grey: "#CCCCCC"
+    grey: "#CCCCCC",
+    teal: "A7EDE7"
 }
 
 export default COLORS;
