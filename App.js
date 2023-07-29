638db0dde0e2bf0c2b7b68fd0f73dff8854889e6
Vedant Bakshi <vedantbakshi07@gmail.com>
2023-07-29 21:57:19 +0530

    COM-2 added login & signup ui few minor fixes needed later

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import LoginScreen from './screens/LoginScreen';
+import RegistrationScreen from './screens/RegistrationScreen';
+import HomeScreen from './screens/HomeScreen';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Loader from './components/Loader';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  const [initialRouteName, setInitialRouteName] = React.useState('');
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      authUser();
+    }, 2000);
+  }, []);
+
+  const authUser = async () => {
+    try {
+      let userData = await AsyncStorage.getItem('userData');
+      if (userData) {
+        userData = JSON.parse(userData);
+        if (userData.loggedIn) {
+          setInitialRouteName('HomeScreen');
+        } else {
+          setInitialRouteName('LoginScreen');
+        }
+      } else {
+        setInitialRouteName('RegistrationScreen');
+      }
+    } catch (error) {
+      setInitialRouteName('RegistrationScreen');
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      {!initialRouteName ? (
+        <Loader visible={true} />
+      ) : (
+        <>
+          <Stack.Navigator
+            initialRouteName={initialRouteName}
+            screenOptions={{headerShown: false}}>
+            <Stack.Screen
+              name="RegistrationScreen"
+              component={RegistrationScreen}
+            />
+            <Stack.Screen name="LoginScreen" component={LoginScreen} />
+            <Stack.Screen name="HomeScreen" component={HomeScreen} />
+          </Stack.Navigator>
+        </>
+      )}
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
